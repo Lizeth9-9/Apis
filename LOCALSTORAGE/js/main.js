ec05f57a2626ae32669ba5763332b1f48d546cae
Lizeth9-9 <lili.prog0@gmail.com>
2026-07-16 13:48:15 +0200

    boton me gusta

--- LOCALSTORAGE/js/main.js
+++ LOCALSTORAGE/js/main.js
@@ -1,21 +1,52 @@
-async function obtenerImg() {
+async function galeria() {
     try {
-        const respuesta = await fetch('https://api.unsplash.com/search/photos?query=nature&client_id=cknF6p-DUqn7m70r9nkgLVBxrho-bXopa3TrlbCqVo4');
+        // edpoint predefinido
+        /*         const respuesta = await fetch('https://api.unsplash.com/search/photos?query=nature&client_id=cknF6p-DUqn7m70r9nkgLVBxrho-bXopa3TrlbCqVo4'); */
+
+        // edpoint con 20 img
+        const respuesta = await fetch('https://api.unsplash.com/search/photos?query=nature&per_page=20&client_id=cknF6p-DUqn7m70r9nkgLVBxrho-bXopa3TrlbCqVo4');
+
+
+
         const datos = await respuesta.json();
         console.log(datos);
 
-        const container = document.querySelector('.container')
+        const container = document.querySelector('.container');
         datos.results.forEach(element => {
             // console.log(element.urls.regular);
 
-            const card = document.createElement('div')
-            card.classList.add('card');
+            const card = document.createElement('div');
+            const card2 = document.createElement('div');
+
+            card2.classList.add('card');
             const img = document.createElement('img');
             img.src = element.urls.regular;
 
-            card.appendChild (img)
-            container.appendChild(card)
+            const fotografo = document.createElement('h1');
+            fotografo.textContent = element.user.name;
+
+            const boton = document.createElement('button');
+
+            boton.addEventListener('click', () => {
+                boton.classList.toggle('activo');
+                if (boton.classList.contains('activo')) {
+                    boton.textContent = '❤️';
+                } else {
+                    boton.textContent = '🤍';
+                }
+                console.log(element.id);
+
+            });
+
+            card2.appendChild(img)
+            card2.appendChild(fotografo)
+            card2.appendChild(boton)
+            container.appendChild(card2)
         });
+
+        function guardarFavoritos() {
+
+        }
 
 
     } catch (error) {
@@ -23,4 +54,4 @@ async function obtenerImg() {
         alert('No se pudieron cargar los productos.');
     }
 }
-obtenerImg();
+galeria();
