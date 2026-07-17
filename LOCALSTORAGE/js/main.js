7e55731e992399dc75c6674a9e1b6bd585dec619
Lizeth9-9 <lili.prog0@gmail.com>
2026-07-17 11:25:23 +0200

    favoritos local store ok

--- LOCALSTORAGE/js/main.js
+++ LOCALSTORAGE/js/main.js
@@ -2,14 +2,16 @@ async function galeria() {
     try {
         // edpoint predefinido
         /*         const respuesta = await fetch('https://api.unsplash.com/search/photos?query=nature&client_id=cknF6p-DUqn7m70r9nkgLVBxrho-bXopa3TrlbCqVo4'); */
-
         // edpoint con 20 img
         const respuesta = await fetch('https://api.unsplash.com/search/photos?query=nature&per_page=20&client_id=cknF6p-DUqn7m70r9nkgLVBxrho-bXopa3TrlbCqVo4');
-
-
-
         const datos = await respuesta.json();
         console.log(datos);
+
+        const state = {
+            fotos: datos.results,
+            favoritos: JSON.parse(localStorage.getItem('favoritosPhotos')) || []
+        };
+
 
         const container = document.querySelector('.container');
         datos.results.forEach(element => {
@@ -18,7 +20,9 @@ async function galeria() {
             const card = document.createElement('div');
             const card2 = document.createElement('div');
 
-            card2.classList.add('card');
+            card.classList.add('card');
+            card2.classList.add('card2');
+
             const img = document.createElement('img');
             img.src = element.urls.regular;
 
@@ -27,8 +31,17 @@ async function galeria() {
 
             const boton = document.createElement('button');
 
+            if (state.favoritos.includes(element.id)) {
+                boton.classList.add('activo');
+                boton.textContent = '❤️';
+            }
+            else {
+                boton.textContent = '🤍';
+            }
+
             boton.addEventListener('click', () => {
                 boton.classList.toggle('activo');
+                toggleFavorito(element.id);
                 if (boton.classList.contains('activo')) {
                     boton.textContent = '❤️';
                 } else {
@@ -41,13 +54,27 @@ async function galeria() {
             card2.appendChild(img)
             card2.appendChild(fotografo)
             card2.appendChild(boton)
-            container.appendChild(card2)
+            card.appendChild(card2)
+
+            container.appendChild(card)
         });
 
         function guardarFavoritos() {
+            localStorage.setItem('favoritosPhotos', JSON.stringify(state.favoritos))
+            console.log(localStorage);
 
         }
 
+        function toggleFavorito(id) {
+            const index = state.favoritos.indexOf(id);
+            if (index === -1) {
+                state.favoritos.push(id);
+            }
+            else {
+                state.favoritos.splice(index, 1);
+            }
+            guardarFavoritos();
+        }
 
     } catch (error) {
         console.error('Error al cargar los datos:', error.message);
